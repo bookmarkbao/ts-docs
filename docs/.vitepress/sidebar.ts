215e905bef39cc86ad8e044e2ade62941f0ad05a
daxiang <2890594972@qq.com>
2024-12-13 11:21:37 +0800

    feat:ignore .vitepress

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,5 +1,7 @@
 export default [
   { text: "dify", link: "/dify/index" },
+  { text: "Step 1: Installation", link: "/lokijs/index" },
+  { text: "Overview", link: "/lokijs/lokijs-indexedDB-MongoDb" },
   { text: "How to Create a Python Project and Virtual Environment", link: "/python/how to create python project and venv" },
   { text: "tts", link: "/tts/index" },
 ];
